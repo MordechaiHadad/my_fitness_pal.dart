4bd4e0c73ee794c1f87a5cb4b2d99a84e9521f6b
morde <mordechai.hadad01@gmail.com>
2022-06-22 18:02:23 +0300

    first commit

---
+++ lib/my_fitness_pal.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library my_fitness_pal;
+
+export 'src/my_fitness_pal_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
---
+++ test/my_fitness_pal_test.dart
@@ -0,0 +1,16 @@
+import 'package:my_fitness_pal/my_fitness_pal.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
