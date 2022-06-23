50076fb41dc71b0347f2197335072a4ebaf4dec2
morde <mordechai.hadad01@gmail.com>
2022-06-23 13:46:05 +0300

    added: Base client builder, with Languages enum

--- example/my_fitness_pal_example.dart
+++ example/my_fitness_pal_example.dart
@@ -1,6 +1,6 @@
 import 'package:my_fitness_pal/my_fitness_pal.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+  var client = Client("token")..withLanguage(Language.brazilian);
+  print(client.language);
 }
--- lib/src/my_fitness_pal_base.dart
+++ lib/src/my_fitness_pal_base.dart
@@ -1,6 +1,64 @@
 // TODO: Put public facing types in this file.
 
+import 'dart:io';
+
 /// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+
+class Client {
+  late final HttpClient _client;
+  late final String _language;
+  late final String _token;
+
+  Client(String token) {
+    _token = token;
+    _client = HttpClient();
+  }
+
+  void withLanguage(Language language) {
+    switch(language) {
+      case Language.english:
+        _language = "en-US";
+        break;
+      case Language.spanish:
+        _language = "es";
+        break;
+      case Language.italian:
+        _language = "it";
+        break;
+      case Language.german:
+        _language = "de";
+        break;
+      case Language.french:
+        _language = "fr";
+        break;
+      case Language.brazilian:
+        _language = "pl";
+        break;
+      case Language.swedish:
+        _language = "sv";
+        break;
+      case Language.danish:
+        _language = "da";
+        break;
+      case Language.norwegian:
+        _language = "no";
+        break;
+    }
+  }
+
+  String get language {
+    return _language;
+  }
+}
+
+enum Language {
+  english,
+  spanish,
+  italian,
+  german,
+  french,
+  brazilian,
+  swedish,
+  danish,
+  norwegian,
 }
